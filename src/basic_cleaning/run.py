2482f155fd9e3d515fb7821831ee432243b0f072
Luis Alfredo Leon Villapun <luisalfredoleonvillapun@MacBook-Air-de-Luis.local>
2023-09-12 16:55:01 +0200

    Added extra data cleaning step for geolocation

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -36,6 +36,10 @@ def go(args):
     df = df[idx].copy()
     df['last_review'] = pd.to_datetime(df['last_review'])
 
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5,
+                                                                           41.2)
+    df = df[idx].copy()
+
     # Save the clean df and upload to W&B
     logging.info("Saving clean sample into csv.")
     df.to_csv(args.output_artifact, index=False)
